2049b5dc813d762be943d00638c8bd676bf925fc
Cristina Luengo <cristinaluengoagullo@gmail.com>
2016-05-14 16:45:48 +0200

    Rotation for LP (now it works)

--- Project LP/src/boxwrapping.cpp
+++ Project LP/src/boxwrapping.cpp
@@ -42,7 +42,7 @@ int main(int argc, char* argv[]) {
 	int nBoxes = atoi(tokens[0].c_str());
 	for(int j = i; j < nBoxes+i; j++) {
 	  boxes.push_back(make_pair(width,height));
-	  maxLength += max(width,height);
+	  maxLength += height;//max(width,height);
 	}
 	i += nBoxes;
       }
@@ -53,56 +53,57 @@ int main(int argc, char* argv[]) {
   sort(boxes.begin(),boxes.end(),compareBoxes);
   IloEnv env;
   try {
-    IloIntVarArray x_tl(env,boxes.size(),0,w-1);
-    IloIntVarArray x_br(env,boxes.size(),0,w-1);
-    IloIntVarArray y_tl(env,boxes.size(),0,maxLength);
-    IloIntVarArray y_br(env,boxes.size(),0,maxLength);
-    IloExpr length(env);
+    IloNumVarArray x_tl(env,boxes.size(),0,w-1);
+    IloNumVarArray y_tl(env,boxes.size(),0,maxLength-1);
+    IloNumVarArray width(env,boxes.size(),0,w);
+    IloNumVarArray height(env,boxes.size(),0,maxLength-1);
+    IloNumVar length(env,0,maxLength-1);
     IloModel model(env);
+    x_tl[0].setBounds(0,0);
+    y_tl[0].setBounds(0,0);
     for(IloInt i = 0; i < boxes.size(); i++) {
       int boxWidth = boxes[i].first;
       int boxHeight = boxes[i].second;
-      IloIntVar finalWidth(env);
-      IloIntVar finalHeight(env);
-      model.add(finalWidth == boxWidth || finalWidth == boxHeight);
-      model.add(finalHeight == boxHeight || finalHeight == boxWidth);
-      if(boxWidth != boxHeight) 
-	model.add(finalWidth != finalHeight);
-      if(i == 0) 
-	model.add(x_tl[0] <= 1/2*(w - finalWidth));
-      model.add(x_tl[i] <= w-finalWidth);
-      model.add(x_tl[i] <= x_br[i]);
-      model.add(y_tl[i] <= y_br[i]);
-      model.add((x_br[i] == x_tl[i]+finalWidth-1));
-      model.add((y_br[i] == y_tl[i]+finalHeight-1));
-      for(IloInt j = i+1; j < boxes.size(); j++) {
-	if(boxes[j].first*boxes[j].second == boxes[i].first*boxes[i].second)
-	  model.add((x_tl[j] >= x_br[i]+1) || (y_tl[j] >= y_br[i]+1)); 
-	// Two boxes cannot be overlapped in the paper roll. 
-	else 
-	  model.add((x_tl[j] >= x_br[i]+1) || (x_br[j] <= x_tl[i]-1) || (y_tl[j] >= y_br[i]+1) || (y_br[j] <= y_tl[i]-1));
+      if(boxWidth != boxHeight) {
+	model.add(width[i] == boxWidth || width[i] == boxHeight);
+	model.add(height[i] == boxHeight || height[i] == boxWidth);
+	model.add(width[i] != height[i]);
       }
+      else {
+	width[i].setBounds(boxWidth,boxWidth);
+	height[i].setBounds(boxHeight,boxHeight);
+      }
+      model.add(x_tl[i]+width[i] <= w);
+      for(IloInt j = i+1; j < boxes.size(); j++) {
+	if(boxes[j].first == boxes[i].first && boxes[j].second == boxes[i].second) {
+	  model.add((x_tl[j] >= x_tl[i]+width[i]) || (y_tl[j] >= y_tl[i]+height[i]));
+	} 
+	// Two boxes cannot be overlapped in the paper roll. 
+	else
+	  model.add((x_tl[j]+width[j] <= x_tl[i]) || (x_tl[j] >= x_tl[i]+width[i]) || (y_tl[j]+height[j] <= y_tl[i]) || (y_tl[j] >= y_tl[i]+height[i]));
+      }
+      model.add(length >= y_tl[i]+height[i]-1);
     }
-    length = IloMax(y_br)+1;
     model.add(IloMinimize(env,length));
     IloCplex cplex(model);
     cplex.setOut(env.getNullStream());
     if (cplex.solve()) {
-      cout << cplex.getObjValue() << endl;
+      cout << cplex.getObjValue()+1 << endl;
       for (IloInt i = 0; i < boxes.size(); i++) {
-	IloInt xtl = cplex.getValue(x_tl[i]);
-	IloInt ytl = cplex.getValue(y_tl[i]);
-	IloInt xbr = cplex.getValue(x_br[i]);
-	IloInt ybr = cplex.getValue(y_br[i]);
+	IloInt xtl = int(cplex.getValue(x_tl[i])+0.5);
+	IloInt ytl = int(cplex.getValue(y_tl[i])+0.5);
+	IloInt w = int(cplex.getValue(width[i])+0.5);
+	IloInt h = int(cplex.getValue(height[i])+0.5);
 	if(xtl < 1) cout << "0 ";
 	else cout << xtl << " ";
 	if(ytl < 1) cout << "0    ";
 	else cout << ytl << "    ";
-	if(xbr < 1) cout << "0 ";
-	else cout << xbr << " ";
-	if(ybr < 1) cout << "0 " << endl;
-	else cout << ybr << " " << endl;	
+	if(xtl < 1) cout << w-1 << " ";
+	else cout << xtl+w-1 << " ";
+	if(ytl < 1) cout << h-1 << ""/* -> " << w << "x" << h*/ << endl;	
+	else cout << ytl+h-1 << " "/*-> " << w << "x" << h*/ << endl;	
       }
+      //cplex.exportModel("model.lp");
     }
     else {
       cout << " No solution found" << endl;
